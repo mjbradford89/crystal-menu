63e9e8ef14cf09f93e633ba150c7067261801398
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-09-14 17:22:13 -0700

    Add documentation

--- src/Menu.js
+++ src/Menu.js
@@ -9,32 +9,57 @@ import './Menu.soy';
 import './MenuItem.js';
 import 'bower:steel-dropdown/src/Dropdown';
 
+
+/**
+ * Menu component.
+ */
 class Menu extends SoyComponent {
+	/**
+	 * @inheritDoc
+	 */
 	constructor(opt_config) {
 		super(opt_config);
 		this.eventHandler_ = new EventHandler();
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	attached() {
 		super.attached();
 		this.eventHandler_.add(dom.on(document, 'click', this.handleDocClick_.bind(this)));
 	}
 
+	/**
+	 * @inheritDoc
+	 */
+	detached() {
+		super.detached();
+		this.eventHandler_.removeAllListeners();
+	}
+
+	/**
+	 * Closes the Menu.
+	 */
 	close() {
 		dom.removeClasses(this.element, 'open');
 
 		dom.removeClasses(this.getMenuItemSubmenu_(), Menu.MENU_ITEM_SUBMENU_OPEN);
 	}
 
-	detached() {
-		super.detached();
-		this.eventHandler_.removeAllListeners();
-	}
-
+	/**
+	 * Gets the menu-item-submenu element.
+	 * @protected
+	 */
 	getMenuItemSubmenu_() {
 		return this.element.querySelector('.' + Menu.MENU_ITEM_SUBMENU + ' .dropdown-menu');
 	}
 
+	/**
+	 * Handles document click in order to hide menu.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleDocClick_(event) {
 		if (this.element.contains(event.target)) {
 			return;
@@ -42,6 +67,11 @@ class Menu extends SoyComponent {
 		this.close();
 	}
 
+	/**
+	 * Handles item click and fires itemSelected event.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleItemClick_(event) {
 		var menuItem = event.handledByComponent;
 
@@ -52,12 +82,18 @@ class Menu extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Opens the menu.
+	 */
 	open() {
 		dom.addClasses(this.element, 'open');
 
 		dom.addClasses(this.getMenuItemSubmenu_(), Menu.MENU_ITEM_SUBMENU_OPEN);
 	}
 
+	/**
+	 * Toggles the menu, closing it when open or opening it when closed.
+	 */
 	toggle() {
 		if (!this.disabled) {
 			dom.toggleClasses(this.element, 'open');
@@ -67,30 +103,75 @@ class Menu extends SoyComponent {
 	}
 }
 
+/**
+ * Attrbutes definition.
+ * @type {!Object}
+ * @static
+ */
 Menu.ATTRS = {
+	/**
+	 * Boolean controlling weather the menu is disabled or enabled.
+	 * @type {!boolean}
+	 * @default false
+	 */
 	disabled: {
 		validator: core.isBoolean,
 		value: false
 	},
 
+	/**
+	 * A list representing the Menu items.
+	 * @type {!Array<!MenuItem>|!Array{!Object}}
+	 * @default []
+	 */
 	items: {
 		validator: Array.isArray,
 		value: []
 	},
 
+	/**
+	 * A string representing the layout mode of the menu. Can either be
+	 * 'overlay' or 'inline'.
+	 * @type {string}
+	 * @default 'overlay'
+	 */
 	layoutMode: {
 		validator: core.isString,
 		value: 'overlay'
 	},
 
+	/**
+	 * A string representing the position of the menu. Can either be 'down' or
+	 * 'up'.
+	 * @type {string}
+	 * @default 'overlay'
+	 */
 	position: {
 		validator: core.isString,
 		value: 'down'
 	}
 };
 
+/**
+ * Default menu elementClasses.
+ * @default menu
+ * @type {string}
+ * @static
+ */
 Menu.ELEMENT_CLASSES = 'menu';
+
+/**
+ * The CSS class added to submenus.
+ * @type {string}
+ * @static
+ */
 Menu.MENU_ITEM_SUBMENU = 'menu-item-submenu';
+
+/**
+ * The CSS class added to open submenus.
+ * @type {string}
+ * @static
+ */
 Menu.MENU_ITEM_SUBMENU_OPEN = 'menu-item-submenu-open';
 
 ComponentRegistry.register('Menu', Menu);
--- src/MenuItem.js
+++ src/MenuItem.js
@@ -10,36 +10,62 @@ import './Menu.js';
  * MenuItem component.
  */
 class MenuItem extends SoyComponent {
+	/**
+	 * @inheritDoc
+	 */
 	constructor(opt_config) {
 		super(opt_config);
 	}
 }
 
 /**
- * Default menuitem elementClasses.
- * @default menuitem
- * @type {String}
+ * Default menu elementClasses.
+ * @default menu
+ * @type {string}
  * @static
  */
 MenuItem.ELEMENT_CLASSES = 'menuitem';
+
+/**
+ * Default menu tag name.
+ * @default li
+ * @type {string}
+ * @static
+ */
 MenuItem.ELEMENT_TAG_NAME = 'li';
 
 /**
- * MenuItem attributes definition.
- * @type {Object}
+ * Attrbutes definition.
+ * @type {!Object}
  * @static
  */
 MenuItem.ATTRS = {
+	/**
+	 * Boolean controlling weather the menu item is disabled or enabled.
+	 * @type {!boolean}
+	 * @default false
+	 */
 	disabled: {
 		validator: core.isBoolean,
 		value: false
 	},
 
+	/**
+	 * Boolean controlling weather the menu item is a divider.
+	 * @type {!boolean}
+	 * @default false
+	 */
 	divider: {
 		validator: core.isBoolean,
 		value: false
 	},
 
+	/**
+	 * Object representing the submenu of this menu item. controlling weather
+	 * the menu item is a divider.
+	 * @type {!Object|!Menu}
+	 * @default null
+	 */
 	submenu: {
 		validator: function(val) {
 			return core.isObject(val);
