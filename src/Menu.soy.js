59976a1a83b32751e67d21be6c9bc323612a82bd
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-09-15 10:07:18 -0700

    Remove some unused leftover styles

--- src/Menu.soy.js
+++ src/Menu.soy.js
@@ -21,17 +21,17 @@ if (typeof Templates.Menu == 'undefined') { Templates.Menu = {}; }
 Templates.Menu.content = function(opt_data, opt_ignored, opt_ijData) {
   var output = '';
   var hasItems__soy3 = opt_data.items && opt_data.items.length > 0;
-  output += '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="menu-' + soy.$$escapeHtmlAttribute(opt_data.layoutMode) + soy.$$escapeHtmlAttribute(hasItems__soy3 ? '' : ' no-menu-items') + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '">';
-  var param11 = '';
+  output += '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="menu-' + soy.$$escapeHtmlAttribute(opt_data.layoutMode) + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '">';
+  var param10 = '';
   if (hasItems__soy3) {
-    var itemList14 = opt_data.items;
-    var itemListLen14 = itemList14.length;
-    for (var itemIndex14 = 0; itemIndex14 < itemListLen14; itemIndex14++) {
-      var itemData14 = itemList14[itemIndex14];
-      param11 += Templates.MenuItem.content(soy.$$augmentMap(itemData14, {id: opt_data.id + '-item-' + itemIndex14, menuId: opt_data.id, layoutMode: opt_data.layoutMode}), null, opt_ijData);
+    var itemList13 = opt_data.items;
+    var itemListLen13 = itemList13.length;
+    for (var itemIndex13 = 0; itemIndex13 < itemListLen13; itemIndex13++) {
+      var itemData13 = itemList13[itemIndex13];
+      param10 += Templates.MenuItem.content(soy.$$augmentMap(itemData13, {id: opt_data.id + '-item-' + itemIndex13, menuId: opt_data.id, layoutMode: opt_data.layoutMode}), null, opt_ijData);
     }
   }
-  output += soy.$$escapeHtml(Templates.Dropdown.content({body: soydata.VERY_UNSAFE.$$ordainSanitizedHtmlForInternalBlocks(param11), header: soydata.VERY_UNSAFE.$$ordainSanitizedHtmlForInternalBlocks('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-header" class="menu-item-content" data-onclick="' + soy.$$escapeHtmlAttribute(opt_data.id) + ':toggle"><a>' + soy.$$escapeHtml(opt_data.header) + '</a>' + ((hasItems__soy3) ? '<div class="menu-item-submenu-arrow"><span class="caret"></span></div>' : '') + '</div>'), id: opt_data.id + '-dropdown', elementClasses: 'menu-item-submenu', position: opt_data.position}, null, opt_ijData));
+  output += soy.$$escapeHtml(Templates.Dropdown.content({body: soydata.VERY_UNSAFE.$$ordainSanitizedHtmlForInternalBlocks(param10), header: soydata.VERY_UNSAFE.$$ordainSanitizedHtmlForInternalBlocks('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-header" class="menu-item-content" data-onclick="' + soy.$$escapeHtmlAttribute(opt_data.id) + ':toggle"><a>' + soy.$$escapeHtml(opt_data.header) + '</a>' + ((hasItems__soy3) ? '<div class="menu-item-submenu-arrow"><span class="caret"></span></div>' : '') + '</div>'), id: opt_data.id + '-dropdown', elementClasses: 'menu-item-submenu', position: opt_data.position}, null, opt_ijData));
   output += '</div>';
   return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
 };
