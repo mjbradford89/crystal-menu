c2fb8df9f0faaaa3988312fddda30a242e850180
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-09-16 15:26:13 -0700

    configure karma for tests

--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
 		files: [
 			'bower_components/soyutils/soyutils.js',
 			'bower_components/metal/src/**/*.js',
+			'bower_components/steel*/src/**/*.js',
 			'src/**/*.js',
 			'test/**/*.js'
 		],
@@ -21,6 +22,7 @@ module.exports = function (config) {
 			'src/**/!(*.soy).js': ['coverage', 'commonjs'],
 			'src/**/*.soy.js': ['babel', 'commonjs'],
 			'bower_components/metal/**/*.js': ['babel', 'commonjs'],
+			'bower_components/steel*/**/*.js': ['babel', 'commonjs'],
 			'test/**/*.js': ['babel', 'commonjs']
 		},
 
--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
 		files: [
 			'bower_components/soyutils/soyutils.js',
 			'bower_components/metal/src/**/*.js',
+			'bower_components/steel*/src/**/*.js',
 			'src/**/*.js',
 			'test/**/*.js'
 		],
@@ -19,6 +20,7 @@ module.exports = function (config) {
 		preprocessors: {
 			'src/**/*.js': ['babel', 'commonjs'],
 			'bower_components/metal/**/*.js': ['babel', 'commonjs'],
+			'bower_components/steel*/**/*.js': ['babel', 'commonjs'],
 			'test/**/*.js': ['babel', 'commonjs']
 		},
 
